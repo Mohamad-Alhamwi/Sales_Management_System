8dae2637db905517f724a6dcbb7c7f07ab905811
Mohamad-Alhawmi <moda.tnt.edu@outlook.com>
2021-05-21 12:44:28 +0300

    Added salesperson model

---
+++ models/salesperson.js
@@ -0,0 +1,80 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../config/db');
+
+const Salesperson = sequelize.define(
+    'salesperson',
+    {
+        salesperson_id:
+        {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        first_name: 
+        {
+            type:  Sequelize.STRING,
+            allowNull: false           
+        },
+        last_name:
+        {
+            type:  Sequelize.STRING,
+            allowNull: false
+        },
+        email:
+        {
+            type:  Sequelize.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password:
+        {
+            type:  Sequelize.STRING,
+            allowNull: false
+        },
+        gender:
+        {
+            type:  Sequelize.STRING,
+            allowNull: true
+        },
+        phone_number:
+        {
+            type:  Sequelize.STRING,
+            allowNull: true,
+            unique: true
+        },
+        birth_date:
+        {
+            type:  Sequelize.DATE,
+            allowNull: true
+        },
+        country:
+        {
+            type:  Sequelize.STRING,
+            allowNull: true
+        },
+        city:
+        {
+            type:  Sequelize.STRING,
+            allowNull: true
+        },
+        town:
+        {
+            type:  Sequelize.STRING,
+            allowNull: true
+        },
+        postal_code:
+        {
+            type:  Sequelize.STRING,
+            allowNull: true
+        },
+        img:
+        {
+            type:  Sequelize.BLOB,
+            allowNull: true
+        }
+    }
+);
+
+module.exports = Salesperson;
