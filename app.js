3931ba2a6c144154cd757166857a10946fe4b95d
Mohamad-Alhawmi <moda.tnt.edu@outlook.com>
2021-05-21 18:06:30 +0300

    Added middleware to the app to be able to parse the body of the request.

--- app.js
+++ app.js
@@ -18,12 +18,25 @@ const salespersons = require('./routes/salespersons');
 dotenv.config();
 
 
+// Models 
+const Salesperson = require('./models/salesperson') ;
+
+
 // 
 const sequelize = require('./config/db');
 
 
 // Creating the express application.
 const app = express();
+
+
+// Configure the app to be able to parse the body of the request.
+app.use(express.urlencoded(
+{
+    extended: true
+}));
+
+app.use(express.json());
 
 
 // Serving static files.
@@ -43,6 +56,16 @@ app.use('/salespersons', salespersons);
 // Creating PORT to run the app on.
 const PORT = process.env.PORT || 5000;
 
+// Syncing table to database.
+sequelize.sync()
+.then(result =>
+{
+    // Taking app object and listen to it.
+    app.listen(PORT, '0.0.0.0', console.log(`Server is running on port ${PORT}`));
+})
+.catch(err =>
+{
+    console.log(err);
+});
 
-// Taking app object and listen to it.
-app.listen(PORT, '0.0.0.0', console.log(`Server is running on port ${PORT}`));
+
