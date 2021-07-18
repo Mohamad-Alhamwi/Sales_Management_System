3dc0806326e0c3d01ca9b63916e9d503cb15d762
Mohamad-Alhawmi <moda.tnt.edu@outlook.com>
2021-07-18 22:40:40 +0300

    Added routes for side menu links.

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,12 +8,98 @@ const page_style = "../../css/dashboard.css";
 router.get('/statistics', ensureAuthenticated, (req, res) =>
 {
     let page_title = "Statistics";
+    let path = "/dashboard/statistics";
 
     res.render(
     'dashboard', 
     {
         page_title,
-        page_style
+        page_style,
+        path
+    });
+});
+
+router.get('/customers', ensureAuthenticated, (req, res) =>
+{
+    let page_title = "Customers";
+    let path = "/dashboard/customers";
+
+    res.render(
+    'dashboard', 
+    {
+        page_title,
+        page_style,
+        path
+    });
+});
+
+router.get('/services', ensureAuthenticated, (req, res) =>
+{
+    let page_title = "Services";
+    let path = "/dashboard/services";
+
+    res.render(
+    'dashboard', 
+    {
+        page_title,
+        page_style,
+        path
+    });
+});
+
+router.get('/products', ensureAuthenticated, (req, res) =>
+{
+    let page_title = "Products";
+    let path = "/dashboard/products";
+
+    res.render(
+    'dashboard', 
+    {
+        page_title,
+        page_style,
+        path
+    });
+});
+
+router.get('/visits', ensureAuthenticated, (req, res) =>
+{
+    let page_title = "Visits";
+    let path = "/dashboard/visits";
+
+    res.render(
+    'dashboard', 
+    {
+        page_title,
+        page_style,
+        path
+    });
+});
+
+router.get('/orders', ensureAuthenticated, (req, res) =>
+{
+    let page_title = "Orders";
+    let path = "/dashboard/orders";
+
+    res.render(
+    'dashboard', 
+    {
+        page_title,
+        page_style,
+        path
+    });
+});
+
+router.get('/bills', ensureAuthenticated, (req, res) =>
+{
+    let page_title = "Bills";
+    let path = "/dashboard/bills";
+
+    res.render(
+    'dashboard', 
+    {
+        page_title,
+        page_style,
+        path
     });
 });
 
